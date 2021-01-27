1bd6ca5c374925021acb0658ed04a77c31178746
pedrogussi <pedrogussi@hotmail.com>
2021-01-27 00:34:28 -0300

    conection on mongoDB Atlas

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+
+import 'dotenv/config.js'
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database established')
+})
+
+app.listen(port, () => {
+    console.log(`Noseless Server is running, on port: ${port}`);
+})
