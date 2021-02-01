a0e22dcaa5ccf79a2515941aba29c30bcbf11fda
pedrogussi <pedrogussi@hotmail.com>
2021-02-01 18:05:35 -0300

    começando a construção do frontend

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+
+export default class CreateExercise  extends Component {
+    render () {
+        return (
+            <div>
+                <p>You are in the CreateExercise </p>
+            </div>
+        ) 
+    }
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+
+export default class ExerciseList extends Component {
+    render () {
+        return (
+            <div>
+                <p>You are in the ExerciseList</p>
+            </div>
+        ) 
+    }
+}
